3a6c34ae68672aa97faa35b8f38eb505775558d8
oxynger <oxyner@gmail.com>
2019-05-07 03:11:09 +0400

    add area for work with object

--- src/components/admin_jornal/NewObj.vue
+++ src/components/admin_jornal/NewObj.vue
@@ -5,17 +5,31 @@
         <v-titel class="headline">Выберите шаблон объекта:</v-titel>
       </v-flex>
 
-      <v-flex v-if="itemschems" lg3>
-        <v-select :items="Select()" label="Виды объектов" outline></v-select>
+      <v-flex v-if="ItemScheme" lg3>
+        <v-select :items="SelectTitels()" @change="SetCurrentObject" label="Виды объектов" outline></v-select>
       </v-flex>
     </v-layout>
-    
-    <v-layout align-content-center>
+
+    <v-layout v-for="(field, index) in CurrentItemFilds" v-bind:key="index" align-content-center>
       <v-flex xs6 offset-xs3 lg3>
-        <v-titel class="title">{{currentitem.title}}</v-titel>
+        <v-titel class="title">{{field.title}}:</v-titel>
       </v-flex>
       <v-flex lg3>
-        <v-text-field single-line outline></v-text-field>
+        <v-flex v-if="field.type == 'Bool' ">
+          <input type="checkbox" id="checkbox">
+        </v-flex>
+
+        <v-flex v-if="field.type == 'Int' ">
+          <v-text-field single-line outline></v-text-field>
+        </v-flex>
+
+        <v-flex v-if="field.type == 'Double' ">
+          <v-text-field single-line outline></v-text-field>
+        </v-flex>
+
+        <v-flex v-if="field.type == 'String' ">
+          <v-text-field single-line outline></v-text-field>
+        </v-flex>
       </v-flex>
     </v-layout>
   </v-container>
@@ -25,25 +39,28 @@
 import Axios from "axios";
 export default {
   data: () => ({
-    itemschems: [],
-    currentitem: []
+    ItemScheme: [],
+    CurrentItemFilds: []
   }),
   created() {
     Axios.get("https://rep.moeka.host/api/v1/scheme/item")
       .then(res => {
-        this.itemschems = res.data;
+        this.ItemScheme = res.data;
       })
       .catch(alert);
   },
   methods: {
-    SelectTitel() {
-      return this.itemschems.map(function(item) {
+    SelectTitels() {
+      return this.ItemScheme.map(function(item) {
         return item.title;
       });
     },
-    Select() {
-      return this.itemschems.map(function(item) {
-        return item.fields.title[0];
+    SetCurrentObject(value) {
+      this.ItemScheme.forEach(item => {
+        if (item.title == value) {
+          this.CurrentItemFilds = item.fields;
+          return;
+        }
       });
     }
   }
